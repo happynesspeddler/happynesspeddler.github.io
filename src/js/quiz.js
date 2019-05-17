bc9916b41f88db121e402f9ec1c93ac275725c75
Anurag Shrivastava <anurag145@gmail.com>
2019-05-17 21:07:58 +0530

    v1.0

--- src/js/quiz.js
+++ src/js/quiz.js
@@ -1,67 +1,87 @@
 var questions = [
     "I don’t feel particularly pleased with the way I am.",
     "I am intensely interested in other people.",
-    // "I feel that life is very rewarding",
-    // "I have very warm feelings towards almost everyone.",
-    // "I rarely wake up feeling rested",
-    // "I am not particularly optimistic about the future.",
-    // "I find most things amusing.",
-    // "I am always committed and involved",
-    // "Life is good",
-    // "I do not think that the world is a good place.",
-    // "I laugh a lot",
-    // "I am well satisfied about everything in my life",
-    // "I don’t think I look attractive.",
-    // "There is a gap between what I would like to do and what I have done.",
-    // "I am very happy.",
-    // "I find beauty in some things.",
-    // "I always have a cheerful effect on others.",
-    // "I can fit in (find time for) everything I want to",
-    // "I feel that I am not especially in control of my life.",
-    // "I feel able to take anything on.",
-    // "I feel fully mentally alert.",
-    // "I often experience joy and elation.",
-    // "I don’t find it easy to make decisions.",
-    // "I don’t have a particular sense of meaning and purpose in my life.",
-    // "I feel I have a great deal of energy.",
-    // "I usually have a good influence on events.",
-    // "I don’t have fun with other people.",
-    // "I don’t feel particularly healthy.",
-    // "I don’t have particularly happy memories of the past."
+    "I feel that life is very rewarding.",
+    "I have very warm feelings towards almost everyone.",
+    "I rarely wake up feeling rested.",
+    "I am not particularly optimistic about the future.",
+    "I find most things amusing.",
+    "I am always committed and involved.",
+    "Life is good",
+    "I do not think that the world is a good place.",
+    "I laugh a lot.",
+    "I am well satisfied about everything in my life.",
+    "I don’t think I look attractive.",
+    "There is a gap between what I would like to do and what I have done.",
+    "I am very happy.",
+    "I find beauty in some things.",
+    "I always have a cheerful effect on others.",
+    "I can fit in (find time for) everything I want to.",
+    "I feel that I am not especially in control of my life.",
+    "I feel able to take anything on.",
+    "I feel fully mentally alert.",
+    "I often experience joy and elation.",
+    "I don’t find it easy to make decisions.",
+    "I don’t have a particular sense of meaning and purpose in my life.",
+    "I feel I have a great deal of energy.",
+    "I usually have a good influence on events.",
+    "I don’t have fun with other people.",
+    "I don’t feel particularly healthy.",
+    "I don’t have particularly happy memories of the past."
 ]
 var answers = []
 var holder = 0
-var reverse = [0, 4, 5, 9, 12, 15, 18, 22, 23, 26, 27, 28]
+var reverse = [0,4,5,9,12,13,18,22,23,26,27,28]
 function getSum(total, num) {
     return total + num;
 }
 $(document).ready(function () {
-    $('#start').click(function(){
+    $('#next').click(function () {
+        if (answers.length === holder) {
+           return;
+        }
+        if (questions.length === answers.length) {
+            $('#question').text("Your Result: " + (answers.reduce(getSum) / 29).toFixed(2))
+            $('#question').addClass('complete')
+            $('#start').hide();
+            $('#myForm').hide();
+            $('#quizValue').show();
+            $('#header').text('Interpreting Results').show();
+            $('#interpretation').show();
+            $('#questionLeft').hide();
+            $('#button-next').hide();
+            $('#quizValue').show();
+        } else {
+            holder++
+            $('#questionLeft').text("Question: " + (holder + 1) + "/" + questions.length)
+            $('#question').text(questions[holder])
+            $('input[name="radioName"]').prop('checked', false);
+        }
+
+    })
+
+    $('#start').click(function () {
         $('#guidelines').hide();
         $('#credits').hide();
         $('#quiz').show();
         $('#header').hide()
-        $('#start').html('Next')
+        $('#start').hide();
+        $('#quizValue').hide();
+        $('#button-next').show();
+        $('#questionLeft').text("Question: 1/" + questions.length)
     })
     $('#interpretation').hide();
     $('#quiz').hide();
+    $('#button-next').hide();
     $('#myForm input').on('change', function () {
+        if ((answers.length - 1) === holder) {
+            answers.pop()
+        }
         if (reverse.indexOf(holder) > -1) {
             var temp = +$('input[name=radioName]:checked', '#myForm').val()
             answers.push(6 - temp + 1)
-        } else
-            answers.push(+$('input[name=radioName]:checked', '#myForm').val());
-
-        if (questions.length === answers.length) {
-            $('#question').text("Your Result: "+(answers.reduce(getSum) / 29).toFixed(2))
-            $('#question').addClass('complete')
-            $('#myForm').hide();
-            $('#header').text('Interpreting Results').show();
-            $('#interpretation').show();
         } else {
-            holder++
-            $('#question').text(questions[holder])
-            $('input[name="radioName"]').prop('checked', false);
+            answers.push(+$('input[name=radioName]:checked', '#myForm').val());
         }
     });
 });
